059138882833bb299ff4115fb7756ec22d05ceb9
mishokU <34416536+mishokU@users.noreply.github.com>
2019-05-13 15:09:58 +0300

    Add user activity

---
+++ app/src/main/java/com/example/polyfinderv2/UserActivity.java
@@ -0,0 +1,14 @@
+package com.example.polyfinderv2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_activity);
+    }
+}
